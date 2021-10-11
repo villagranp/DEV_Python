056ad601770ffcef4e06cc0118e7c7b126c9bd6e
Pablo Villagran Torres <pablo.villagran@liztex.com>
2021-10-11 17:06:33 -0600

    committing work to move

--- 5.0 Segundo Curso python/1 Practicas/TipoDatos.py
+++ 5.0 Segundo Curso python/1 Practicas/TipoDatos.py
@@ -60,8 +60,11 @@ print(f"Tu indice de masa corporal es <imc>{imc:.2f}<imc>" )
 
 """
 Ejercicio 8
-Escribir un programa que pida al usuario dos números enteros y muestre por pantalla la <n> entre <m> da un cociente <c> y un resto <r> donde <n> y <m> son los números introducidos por el usuario, y <c> y <r> son el cociente y el resto de la división entera respectivamente.
+Escribir un programa que pida al usuario dos números enteros y muestre por pantalla la <n> entre <m> da un 
+cociente <c> y un resto <r> donde <n> y <m> son los números introducidos por el usuario, y <c> y <r> son el cociente y 
+el resto de la división entera respectivamente.
 """
+
 """
 Ejercicio 9
 Escribir un programa que pregunte al usuario una cantidad a invertir, el interés anual y el número de años, y muestre por pantalla el capital obtenido en la inversión.
